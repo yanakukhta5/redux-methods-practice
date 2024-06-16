4d2ceaf49c744963f415473981f92d54597ac047
Yana Kukhta <kyv@b2bit.pro>
2024-06-16 14:08:37 +0300

    add nested state

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,50 @@ import { useReducer, useEffect } from "react";
 import "./App.css";
 import { DecrementAction, IncrementAction, store } from "./store";
 
-function App() {
+const Counter = ({ counterId }: { counterId: number }) => {
   const [, forseUpdate] = useReducer((x) => x + 1, 0);
 
   useEffect(() => {
     // переданная функция вызывается после обновления состояния
     const unsubscribe = store.subscribe(() => forseUpdate());
     return unsubscribe;
-  }, [])
-
+  }, []);
+  
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() =>
-            store.dispatch({ type: "increment" } satisfies IncrementAction)
-          }
-        >
-          increment
-        </button>
+    <div className="card">
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: "increment",
+            payload: { counterId },
+          } satisfies IncrementAction)
+        }
+      >
+        increment
+      </button>
 
-        <button
-          onClick={() => {
-            store.dispatch({ type: "decrement" } satisfies DecrementAction);
-          }}
-        >
-          decrement
-        </button>
-        <p>count is {store.getState().counter}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button
+        onClick={() => {
+          store.dispatch({
+            type: "decrement",
+            payload: { counterId },
+          } satisfies DecrementAction);
+        }}
+      >
+        decrement
+      </button>
+      <p>count is {store.getState().counters?.[counterId]?.counter}</p>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Counter counterId={1} />
+      <Counter counterId={2} />
+      <Counter counterId={3} />
+    </div>
   );
 }
 
--- src/store.ts
+++ src/store.ts
@@ -6,36 +6,66 @@ type State = {
 
 export type IncrementAction = {
   type: "increment";
+  payload: {
+    counterId: number;
+  };
 };
 
 export type DecrementAction = {
   type: "decrement";
+  payload: {
+    counterId: number;
+  };
 };
 
 type Action = IncrementAction | DecrementAction;
 
-// дефолтное состояние (при первом редюсера вызове его нет)
-const initialState: State = {
+type CounterId = number;
+
+type CountersState = {
+  counters: Record<CounterId, State | undefined>;
+};
+
+// дефолтное состояние каунтера (при первом редюсера вызове его нет)
+const initialCounterState: State = {
   counter: 0,
 };
 
 // описали состояние и экшон и можно создать редюсер
-const reducer = (state: State = initialState, action: Action): State => {
+const reducer = (
+  state: CountersState = {
+    counters: {},
+  },
+  action: Action
+): CountersState => {
+  // иммутабельное обновление
   switch (action.type) {
-    case "increment":
+    case "increment": {
+      const id = action.payload.counterId;
+      const counterObject = state.counters[id] ?? initialCounterState;
+
       return {
-        // иммутабельное обновление
         ...state,
-        counter: ++state.counter,
+        counters: {
+          ...state.counters,
+          [id]: { ...counterObject, counter: counterObject.counter + 1 },
+        },
       };
-    case "decrement":
+    }
+    case "decrement": {
+      const id = action.payload.counterId;
+      const counterObject = state.counters[id] ?? initialCounterState;
+
       return {
-        // иммутабельное обновление
         ...state,
-        counter: --state.counter,
+        counters: {
+          ...state.counters,
+          [id]: { ...counterObject, counter: counterObject.counter - 1 },
+        },
       };
+    }
     default:
-      return state; // иммутабельное обновление
+      return state;
   }
 };
 
